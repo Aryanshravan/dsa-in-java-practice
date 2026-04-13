0eca5095d0a37ee59ed2157cfb1e375291f0d156
Aryanshravan <aryanshravanchouti123@gmail.com>
2026-04-13 10:52:05 +0530

    Second Max in an array

---
+++ Arrays/SecondMaximum.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class SecondMaximum {
+    public static void main(String[] args) {
+        int[] arr = {4,10,10,6,3,8};
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        int tmax = Integer.MIN_VALUE;
+
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] > max) max = arr[i];
+        }
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] > smax && arr[i]!=max) smax = arr[i];
+        }
+        for(int i =0;i< arr.length;i++){
+            if(arr[i] >tmax && arr[i]!=max && arr[i]!= smax) tmax = arr[i];
+        }
+        System.out.println(max);
+        System.out.println(smax);
+        System.out.println(tmax);
+
+    }
+}
